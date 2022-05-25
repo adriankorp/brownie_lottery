e6dbf4bfb583ea359afbad79190b6759eed837a4
adriankorp <adrian.korpowski@gmail.com>
2022-05-25 20:59:30 +0200

    enterLottery function change

--- contracts/lottery.sol
+++ contracts/lottery.sol
@@ -2,15 +2,16 @@
 
 pragma solidity ^0.7.0;
 
-
 contract Lottery {
+    address payable[] public players;
 
+    function enterLottery() public payable{
+        players.push(msg.sender);
+    }
 
-    function enterLottery() public {}
     function getEntranceFee() public {}
+
     function startLottery() public {}
-    function endLottety() public{}
+
+    function endLottety() public {}
 }
-
-
-
