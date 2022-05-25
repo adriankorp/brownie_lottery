7c22b78fa5a4d177ee05f34bd13e3e6f693a7026
adriankorp <adrian.korpowski@gmail.com>
2022-05-25 20:05:26 +0200

    Add functions names in lottery.sol

---
+++ contracts/lottery.sol
@@ -0,0 +1,16 @@
+//SPDX-License-Identifier: UNLICENSED
+
+pragma solidity ^0.7.0;
+
+
+contract Lottery {
+
+
+    function enterLottery() public {}
+    function getEntranceFee() public {}
+    function startLottery() public {}
+    function endLottety() public{}
+}
+
+
+
