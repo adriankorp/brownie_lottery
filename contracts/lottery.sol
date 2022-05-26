e3cc58fc86cdc85d28f8bb2a260fde3fb7ef5d1e
adriankorp <adrian.korpowski@gmail.com>
2022-05-26 18:30:17 +0200

    Change functions and add minUsdEntryCost test

--- contracts/lottery.sol
+++ contracts/lottery.sol
@@ -2,14 +2,29 @@
 
 pragma solidity ^0.7.0;
 
+import "@chainlink/contracts/src/v0.6/interfaces/AggregatorV3Interface.sol";
+
 contract Lottery {
     address payable[] public players;
+    uint256 public minUsdEntry;
+    AggregatorV3Interface internal ethUsdPriceFeed;
 
-    function enterLottery() public payable{
+    constructor(address _priceFeedAddress) public {
+        minUsdEntry = 50 * (10**18);
+        ethUsdPriceFeed = AggregatorV3Interface(_priceFeedAddress);
+    }
+
+    function enterLottery() public payable {
+        require(msg.value > minUsdEntryCost(), "Not enough msg.value");
         players.push(msg.sender);
     }
 
-    function getEntranceFee() public {}
+    function minUsdEntryCost() public view returns (uint256) {
+        (, int256 price, , , ) = ethUsdPriceFeed.latestRoundData();
+        uint256 adjustedPrice = uint256(price) * 10**10;
+        uint256 costToEnter = (minUsdEntry * 10**18) / adjustedPrice;
+        return costToEnter;
+    }
 
     function startLottery() public {}
 
